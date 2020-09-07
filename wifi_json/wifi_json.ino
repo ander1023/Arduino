79dd8926479771518aeb81151c592c9cf7e942a8
大山猛 <ander@MacBook-Pro.local>
2020-09-07 21:55:27 +0800

    wifi_json

--- wifi_json/wifi_json.ino
+++ wifi_json/wifi_json.ino
@@ -1,9 +1,10 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
-#define ssid "id"
-#define psd "password"
+#define ssid "Xiaomi_cmcc"
+#define psd "xiaomi991212"
 #define api "https://v1.hitokoto.cn/?encode=text"
+#define a "http://quan.suning.com/getSysTime.do"
 void setup() {
   // put your setup code here, to run once:
   //初始化串口设置
@@ -26,8 +27,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println(http_post());
-  delay(5000);
-
+  delay(10000);
 }
 
 String  http_post() {
@@ -39,25 +39,25 @@ String  http_post() {
   HTTPClient http;
 
   //配置请求地址
-  http.begin(api); //HTTP请求
-  Serial.print("[HTTP] begin...\n");
+  http.begin(a); //HTTP请求
+//  Serial.print("[HTTP] begin...\n");
 
   //启动连接并发送HTTP报头和报文
   int httpCode = http.POST(" ");
-  Serial.print("[HTTP] POST...\n");
+//  Serial.print("[HTTP] POST...\n");
 
   //连接失败时 httpCode时为负
   if (httpCode > 0) {
 
     //将服务器响应头打印到串口
-    Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+//    Serial.printf("[HTTP] POST... code: %d\n", httpCode);
     //返回string
     if (httpCode == HTTP_CODE_OK) {
       return http.getString();
       
     }
   } else {
-    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+//    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
   }
   //关闭http连接
   http.end();
